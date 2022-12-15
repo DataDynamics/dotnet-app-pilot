6fd19a98b52716175470c43e3ebabe4d95555249
김병곤 <fharenheit@gmail.com>
2022-12-15 23:49:16 +0900

    Add Config.Net.YAML 추가

--- dotnet-app-pilots/DataDynamics.App/Program.cs
+++ dotnet-app-pilots/DataDynamics.App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Reflection;
+using Config.Net;
+using DataDynamics.App.Model;
 using DataDynamics.Common.Spring.Resources;
 using DataDynamics.Common.Spring.Utils;
 using DataDynamics.Common.Utils;
@@ -16,14 +18,22 @@ internal static class Program
     /// 해당 애플리케이션의 주 진입점입니다.
     /// </summary>
     [STAThread]
-    static void Main()
+    static void Main(string[] args)
     {
         IResourceLoader loader = new ConfigurableResourceLoader();
         var resource = loader.GetResource("Config/log4net.config");
-        Console.Out.WriteLine(IOUtils.ToString(resource));
-        Console.Out.WriteLine(resource);
         XmlConfigurator.Configure(resource.File);
 
-        logger.Info(Mediator.Instance.ToString());
+        logger.Info("log4net이 초기화되었습니다.");
+
+        var yamlConfig = loader.GetResource("Config/config.yml");
+
+        logger.Info(string.Format("환경설정 파일 '{0}'을 로딩합니다.", yamlConfig.File.FullName));
+
+        var builder = new ConfigurationBuilder<AppConfig>().UseEnvironmentVariables()
+            .UseYamlFile(yamlConfig.File.FullName);
+        var configuration = builder.Build();
+
+        logger.Info(string.Format("애플리케이션명 : {0}", configuration.Name));
     }
 }
