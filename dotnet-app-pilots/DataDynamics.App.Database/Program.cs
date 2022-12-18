cc00640e55f31c7ba53b7334fbff839bcb4ac876
김병곤 <fharenheit@gmail.com>
2022-12-18 15:08:58 +0900

    데이터베이스 테스트용 애플리케이션 추가

---
+++ dotnet-app-pilots/DataDynamics.App.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DataDynamics.App.Database;
+
+internal static class Program
+{
+    /// <summary>
+    ///     해당 애플리케이션의 주 진입점입니다.
+    /// </summary>
+    [STAThread]
+    private static void Main(string[] args)
+    {
+    }
+}
