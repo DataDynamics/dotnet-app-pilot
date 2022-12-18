b8d5f33fd377fcdd09354f080229b3582e8532ca
김병곤 <fharenheit@gmail.com>
2022-12-18 16:37:20 +0900

    Entity Framework 예제추가

--- dotnet-app-pilots/DataDynamics.App.Database/CustomerRepository.cs
+++ dotnet-app-pilots/DataDynamics.App.Database/CustomerRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataDynamics.App.Database;
@@ -8,11 +10,23 @@ public class CustomerRepository : DbContext
     public DbSet<Customer> Customers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=Clave:123");
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=Clave:123");
+        optionsBuilder.LogTo(Console.WriteLine);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // TODO : 여기에서 Entity를 추가한다.
+        // modelBuilder.Entity<AuditEntry>();
+    }
 }
 
+[Table("customers", Schema = "public")]
 public class Customer
 {
-    public int Id { get; set; }
+    [Column("id")]
+    public long Id { get; set; }
+    [Column("full_name")]
     public string FullName { get; set; }
 }
--- dotnet-app-pilots/DataDynamics.App.Database/Program.cs
+++ dotnet-app-pilots/DataDynamics.App.Database/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SmartSql.Data;
 
 namespace DataDynamics.App.Database;
 
@@ -10,8 +12,43 @@ internal static class Program
     [STAThread]
     private static void Main(string[] args)
     {
-
         string appName = ConfigurationManager.AppSettings["AppName"];
         Console.Out.WriteLine(appName);
+
+        var context = new CustomerRepository();
+        var primaryKey = Random.Shared.NextInt64();
+        var customer = new Customer { Id = primaryKey, FullName = "Pendori" };
+        context.Add(customer);
+        context.SaveChanges();
+
+        context.Remove(customer);
+        context.SaveChanges();
+
+        ///
+        /// ORM으로 호출
+        /// 
+        foreach (var c in context.Customers)
+        {
+            Console.Out.WriteLine(c.FullName);
+        }
+
+        ///
+        /// SQL을 직접 호출
+        /// 
+        var output1 = context.Customers.FromSql($"select * from customers limit 1").ToList();
+        foreach (var c in output1)
+        {
+            Console.Out.WriteLine(c.FullName);
+        }
+
+        ///
+        /// 매개변수 전달
+        /// 
+        var pk = 1;
+        var output2 = context.Customers.FromSql($"select * from customers where id = {pk}").ToList();
+        foreach (var c in output2)
+        {
+            Console.Out.WriteLine(c.FullName);
+        }
     }
 }
